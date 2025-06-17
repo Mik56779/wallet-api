455c523a4de1449d68b84346349d99e6c94dbef3
Mike_Kalange <mikekalange@gmail.com>
2025-06-17 15:53:21 +0200

    Add cron job initialization for production environment

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import GetrateLimiter from "./middleware/rateLimiter.js";
 import router from "./routes/transactionsRoute.js";
 import { initDB } from "./config/db.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 //middlware
+if (process.env.NODE_ENV === "production") job.start();
 app.use(GetrateLimiter);
 app.use(express.json());
 
@@ -16,6 +18,7 @@ app.use("/api/transactions", router);
 
 app.get("/api/health", (req, res) => {
   res.send("its working");
+  res.status(200).json({ status: "ok" });
 });
 
 initDB().then(() => {
